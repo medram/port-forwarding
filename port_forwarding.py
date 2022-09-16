135146ad0515a75fc77868fde47045c217e90ecc
medram <mramouchy@gmail.com>
2022-09-16 14:51:43 +0100

    Updated: use 'selectors' for I/O mutliplexing!

--- port_forwarding.py
+++ port_forwarding.py
@@ -2,8 +2,8 @@
 
 import socket
 import argparse
-import threading
 import logging
+import selectors
 
 
 lhost = '0.0.0.0'       # Default server host (to bind)
@@ -12,7 +12,7 @@ dhost = '127.0.0.1'     # Default destination host.
 dport = 22              # Default destination port.
 BUFFER_SIZE = 4096      # Default buffer size.
 
-threads_list = set()    # Save all theads
+sel = selectors.DefaultSelector()
 
 # Setting up the logger
 fmt = "%(asctime)s - [%(levelname)s]: %(message)s"
@@ -21,61 +21,62 @@ logger = logging.getLogger(__file__)
 
 
 def forward(src, dst):
-    while True:
-        #logger.info('Threads:', threading.enumerate()[1:])
+    try:
         buffer = src.recv(BUFFER_SIZE)
         if buffer:
             dst.sendall(buffer)
         else:
-            break
+            sel.unregister(src)
+            sel.unregister(dst)
 
-    # Close socket connections
+            src.shutdown(socket.SHUT_RDWR)
+            dst.shutdown(socket.SHUT_RDWR)
+
+            src.close()
+            dst.close()
+            logger.info('Connection closed.')
+    except OSError as e:
+        logger.warning('Socket already closed!')
+        logger.warning(e)
+
+
+def accept(server):
     try:
-        src.shutdown(socket.SHUT_RDWR)
-        src.close()
+        client, src_addr = server.accept()
+        client.setblocking(False)
+        # Establishing a cennection to destination.
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((dhost, dport))
+        conn.setblocking(False)
 
-        dst.shutdown(socket.SHUT_RDWR)
-        dst.close()
+        logger.info(
+            f'Connection established: {client.getsockname()} -> ? -> {conn.getpeername()}')
 
-        logger.info('Connection closed.')
-    except OSError:
-        pass  # Connection already shutdown
-    finally:
-        threads_list.remove(threading.current_thread())
+        sel.register(client, selectors.EVENT_READ,
+                     data=(forward, client, conn))
+        sel.register(conn, selectors.EVENT_READ,
+                     data=(forward, conn, client))
+
+    except Exception as e:
+        logger.error(e)
 
 
 def server(lhost, lport, dhost, dport):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.setblocking(False)
         server.bind((lhost, lport))
         server.listen()
 
+        sel.register(server, selectors.EVENT_READ, data=(accept, server))
         logger.info(f'Server started on {lhost}:{lport}')
 
         try:
             while True:
-                try:
-                    client, src_addr = server.accept()
-                    # Establishing a cennection to destination.
-                    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    conn.connect((dhost, dport))
-                    logger.info(
-                        f'Connection established: {client.getsockname()} -> ? -> {conn.getpeername()}')
-
-                    t1 = threading.Thread(
-                        target=forward, args=(client, conn), daemon=True)
-                    t2 = threading.Thread(
-                        target=forward, args=(conn, client), daemon=True)
-
-                    t1.start()
-                    t2.start()
-
-                    # Add new threads to the list
-                    threads_list.add(t1)
-                    threads_list.add(t2)
-
-                except Exception as e:
-                    logger.error(e)
+                events = sel.select()
+                for key, flag in events:
+                    callback, *args = key.data
+                    callback(*args)
         except KeyboardInterrupt:
             logger.info('Server closed')
 
