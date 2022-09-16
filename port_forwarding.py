38bd4f3da94138ca0d173ab7a4bb4d1c29f2a957
medram <mramouchy@gmail.com>
2022-09-16 22:37:37 +0100

    Updated: port forwarding using Asyncio!

--- port_forwarding.py
+++ port_forwarding.py
@@ -3,8 +3,8 @@
 import socket
 import argparse
 import logging
-import selectors
 import traceback
+import asyncio
 
 
 lhost = '0.0.0.0'       # Default server host (to bind)
@@ -14,95 +14,80 @@ dport = 22              # Default destination port.
 BUFFER_SIZE = 4096      # Default buffer size.
 fake_websocket_reply = False
 
-sel = selectors.DefaultSelector()
-
 # Setting up the logger
 fmt = "%(asctime)s - [%(levelname)s]: %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=fmt)
 logger = logging.getLogger(__file__)
 
 
-def websocket_upgrade(client, extra_data):
+async def websocket_upgrade(client):
     """return a fake websocket reply"""
-    buffer = client.recv(BUFFER_SIZE)
+    loop = asyncio.get_event_loop()
+
+    buffer = await loop.sock_recv(client, BUFFER_SIZE)
     if buffer:
-        client.sendall(
-            "HTTP/1.1 101 Switching Protocols (Python)\r\nContent-Length: 1048576000000\r\n\r\n".encode('utf-8'))
+        await loop.sock_sendall(
+            client, "HTTP/1.1 101 Switching Protocols (Python)\r\nContent-Length: 1048576000000\r\n\r\n".encode('utf-8'))
         logger.info('Websocket upgraded!')
 
 
-def forward_ws(client, conn, extra_data):
-    # Upgrading to Websocket.
-    if not extra_data['websocket_handshake']:
-        extra_data['websocket_handshake'] = True
-        return websocket_upgrade(client, extra_data)
-
-    forward(client, conn, extra_data)
-
-
-def forward(src, dst, extra_data):
-    if extra_data['websocket_handshake']:
-        try:
-            buffer = src.recv(BUFFER_SIZE)
-            if buffer:
-                dst.sendall(buffer)
-            else:
-                sel.unregister(src)
-                sel.unregister(dst)
-
-                src.shutdown(socket.SHUT_RDWR)
-                dst.shutdown(socket.SHUT_RDWR)
-
-                src.close()
-                dst.close()
-                logger.info('Connection closed.')
-        except OSError as e:
-            logger.warning('Socket already closed!')
-            logger.warning(e)
+async def tunnel(src, dst):
+    loop = asyncio.get_event_loop()
+    while True:
+        buffer = await loop.sock_recv(src, BUFFER_SIZE)
+        if buffer:
+            await loop.sock_sendall(dst, buffer)
+        else:
+            break
+    logger.info('Connection terminated!')
 
 
-def accept(server):
-    try:
-        client, src_addr = server.accept()
-        client.setblocking(False)
-        # Establishing a cennection to destination.
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect((dhost, dport))
-        conn.setblocking(False)
+async def accept(server):
+    '''Accept server connection'''
+    loop = asyncio.get_event_loop()
 
-        logger.info(
-            f'Connection established: {client.getsockname()} -> ? -> {conn.getpeername()}')
+    client, addr = await loop.sock_accept(server)
+    client.setblocking(False)
+    # Establishing a cennection to destination.
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.setblocking(False)
+    await loop.sock_connect(conn, (dhost, dport))
 
-        extra_data = {'websocket_handshake': False if fake_websocket_reply else True}
+    logger.info(
+        f'Connection established: {client.getsockname()} -> ? -> {conn.getpeername()}')
 
-        sel.register(client, selectors.EVENT_READ,
-                     data=(forward_ws, client, conn, extra_data))
-        sel.register(conn, selectors.EVENT_READ,
-                     data=(forward, conn, client, extra_data))
+    # Fake Websocket upgrade
+    websocket_handshake = False
+    if fake_websocket_reply and not websocket_handshake:
+        await websocket_upgrade(client)
+        websocket_handshake = True
 
-    except ConnectionRefusedError:
-        logger.error('Destination connection refused.')
-    except Exception:
-        logger.error(traceback.format_exc())
+    # piping data
+    loop.create_task(tunnel(client, conn))
+    loop.create_task(tunnel(conn, client))
 
 
-def server(lhost, lport, dhost, dport):
+async def server(lhost, lport, dhost, dport):
+    loop = asyncio.get_event_loop()
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.setblocking(False)
         server.bind((lhost, lport))
         server.listen()
 
-        sel.register(server, selectors.EVENT_READ, data=(accept, server))
         logger.info(f'Server started on {lhost}:{lport}')
-
         try:
             while True:
-                for key, flag in sel.select():
-                    callback, *args = key.data
-                    callback(*args)
-        except KeyboardInterrupt:
-            logger.info('Server closed')
+                await accept(server)
+
+        except ConnectionRefusedError:
+            logger.error('Destination connection refused.')
+        except OSError:
+            logger.error('socket.connect() error!')
+            logger.error(traceback.format_exc())
+        except Exception:
+            logger.error(traceback.format_exc())
 
 
 if __name__ == '__main__':
@@ -137,5 +122,8 @@ if __name__ == '__main__':
     fake_websocket_reply = args.fake_ws_reply
 
     logger.info(f"tunnel: {lhost}:{lport} --> {dhost}:{dport}")
-    # start a server.
-    server(lhost, lport, dhost, dport)
+    try:
+        # start a server.
+        asyncio.run(server(lhost, lport, dhost, dport))
+    except KeyboardInterrupt:
+        logger.info('Server closed')
